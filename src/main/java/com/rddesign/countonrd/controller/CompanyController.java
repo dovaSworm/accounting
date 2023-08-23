0090df0a79f0085b4a067596c8b932d51c89f944
dova <danijeldoveden.55@gmail.com>
2023-08-23 09:49:27 +0200

    react app mora se pokrenuti zasebno

--- src/main/java/com/rddesign/countonrd/controller/CompanyController.java
+++ src/main/java/com/rddesign/countonrd/controller/CompanyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Dova
  */
 @RestController
+@RequestMapping("/api")
 public class CompanyController {
 	@Autowired private CompanyService compService;
 	@PostMapping("/companies")
